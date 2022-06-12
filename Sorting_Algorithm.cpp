ea55681bb15499b29aa43e1a4230b98fd5638fd5
JordanRoberts-2000 <jordanroberts654@outlook.com>
2022-06-12 07:29:42 +0100

    User inputs length, random generated elements

--- Sorting_Algorithm.cpp
+++ Sorting_Algorithm.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <cstdlib>
 #define MAX 100
 
 using namespace std;
 
 int main(){
+    //Input length N
+    int n;
     int arr[MAX];
+    cout<<"Enter a list size"<<endl;
+    cin>>n;
+
+    //Creating random array with length N
+    for(int i = 0; i < n; i++){
+        arr[i] = rand();
+    }
+
+    //Printing created arrow to the console
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+
     return 0;
 }
