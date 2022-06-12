d9c65f2bd415d04d89b8fe93123018746d732727
JordanRoberts-2000 <jordanroberts654@outlook.com>
2022-06-12 07:40:16 +0100

    Sorting algorithm added

--- Sorting_Algorithm.cpp
+++ Sorting_Algorithm.cpp
@@ -17,9 +17,24 @@ int main(){
     }
 
     //Printing created arrow to the console
+
+
+    //Sorting algorithm
+    for(int i = 0; i < n; i++){
+        for(int j = i + 1; j < n; j++){
+            if(arr[j] < arr[i]){
+                //Swap i and j
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
     for(int i = 0; i < n; i++){
         cout << arr[i] << " ";
     }
+    cout << endl;
+
 
     return 0;
 }
